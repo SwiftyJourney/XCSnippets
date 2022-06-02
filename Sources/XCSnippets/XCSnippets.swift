45c5a48f6b9245e4b15d7047e1621bf5009b7abe
Juan Dorado <jfdoradotr@icloud.com>
2022-06-02 18:27:54 -0500

    Fix identation

--- Sources/XCSnippets/XCSnippets.swift
+++ Sources/XCSnippets/XCSnippets.swift
@@ -1,6 +1,6 @@
 public struct XCSnippets {
-    public private(set) var text = "Hello, World!"
+  public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
+  public init() {
+  }
 }
